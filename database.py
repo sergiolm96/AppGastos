7f6bff54ebf8f72ad9b9530b39236902fda52b97
Sergio <sergiolopezmartin96@mgmail.com>
2025-03-17 09:08:38 +0100

    Funcionalidad de registro de gastos y conexion a la base de datos exitosa

---
+++ database.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Inicializa la base de datos
+db = SQLAlchemy()
--- models/expense.py
+++ models/expense.py
@@ -1,16 +1,13 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-
-Base = declarative_base()
-
-class Gasto(Base):
+from database import db  
+class Gasto(db.Model):
     __tablename__ = 'gastos'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    fecha = Column(String, default=datetime.now().strftime('%d-%m-%Y'))
-    concepto = Column(String(100))
-    cantidad = Column(Float, nullable=False)
-    categoria_id = Column(Integer)
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    fecha = db.Column(db.String, default=datetime.now().strftime('%d-%m-%Y'))
+    concepto = db.Column(db.String(100))
+    cantidad = db.Column(db.Float, nullable=False)
+    categoria_id = db.Column(db.Integer)
 
     def __init__(self, fecha, concepto, cantidad, categoria_id):
         self.fecha = fecha
@@ -19,4 +16,4 @@ class Gasto(Base):
         self.categoria_id = categoria_id
 
     def __repr__(self):
-        return "<Gasto(fecha='%s', concepto='%s', cantidad='%s', categoria_id='%s')>" % (self.fecha, self.concepto, self.cantidad, self.categoria_id)
+        return f"<Gasto(fecha='{self.fecha}', concepto='{self.concepto}', cantidad={self.cantidad}, categoria_id={self.categoria_id})>"
