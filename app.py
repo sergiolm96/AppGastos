5e43a0f4bfaa561d8a72b1c99c6fea6eb9f6b293
sergiolm96 <sergiolopezmartin96@gmail.com>
2025-03-17 16:06:42 +0100

    Inicio de sesión implementado

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from database import db
+import os
 from routes.expenses_routes import expense_bp
 from routes.auth_routes import auth_bp
 
@@ -8,6 +9,8 @@ app = Flask(__name__)
 # Configuración de la base de datos (SQLite en este caso)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///expenses.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.secret_key = os.environ.get('SECRET_KEY', 'una_clave_secreta_por_defecto')
 
 # Inicializar la base de datos
 db.init_app(app)
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template, session, redirect, url_for
 from services.user_service import create_user
+from models.user import Usuario
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -20,3 +21,24 @@ def register_user():
             return jsonify(result), 400
         
         return jsonify({"message": "Usuario registrado correctamente"}), 201
+    
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login_user():
+    if request.method == 'GET':
+        return render_template('login.html')
+    
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        username = data.get('nombre_usuario')
+        password = data.get('password')
+
+        if not username or not password:
+            return jsonify({"error": "Faltan credenciales"}), 400
+        
+        user = Usuario.query.filter_by(nombre_usuario=username).first()
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            return jsonify({"message": "Bienvenido"}), 200
+        else:
+            return jsonify({"error": "Credenciales incorrectas"}), 401
